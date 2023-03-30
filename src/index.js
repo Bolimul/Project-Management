3a489d203fe6d6dc1938e30b2c84b21966bfefcd
Nikita_Solonets <nikita.solonets@gmail.com>
2023-03-30 13:42:15 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('A new request has a arrived to index.js');
+    res.send('Hello from the server main page');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port: ${port}`);
+});
