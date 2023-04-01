fbd4e30729c906a83c0a5bdd4f8ab9145e0e3266
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-01 11:32:06 +0300

    Main commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+var username1;
+app.post('/react-app', (req, res) => {
+    var {username, password} = req.body;
+    username1 = username;
+});
+app.get('/react-app', (req, res) => {
+    res.json({message: 'welcome, '+username1});
+    username1 = '';
+})
+app.get('/orange', (req, res) => {
+    res.json({message : 'Hello from the server main page Orange'});
+});
+
+app.listen(port, () => {
+    console.log(`Server is up and running at port: ${port}`);
+});
---
+++ website-react/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+
+function App() {
+  const [username, setName] = React.useState('');
+  const [password, setPass] = React.useState('');
+  const [message, setData] = React.useState('');
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      console.log(e.target.uname.value);
+      console.log(e.target.pass.value);
+      try{
+        axios.post('http://localhost:5000/react-app',{username, password});
+        axios.get('http://localhost:5000/react-app').then(resp => setData(resp.data.message));
+      }catch(error)
+      {
+        console.log(error);
+      }
+        
+    }
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit} action="../../react-app" method="post"> 
+      <h4>Enter nickname</h4>
+        <input type="text" name="uname" placeholder="Username" value={username} onChange={(e) => setName(e.target.value)}/><br/>
+        <h4>Enter password</h4>
+        <input type="password" name="pass" placeholder="password" value={password} onChange={(e) => setPass(e.target.value)}/><br/>
+        <button type="submit">Submit</button> 
+      </form>
+         <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
