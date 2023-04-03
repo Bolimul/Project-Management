9c3d3874ea7348c529fe6ca12102956d2a7962c5
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-03 13:55:28 +0300

    try 1 to give appropriate output

--- server/index.js
+++ server/index.js
@@ -7,24 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-//solving bug with latency getting signal
 var username1 = '';
+const delay = ms =>new Promise(resolve => setTimeout(resolve, ms));
 app.post('/login', (req, res) => {
     var {username, password} = req.body;
     username1 = username;
 });
 app.get('/login', (req, res) => {
-    while(true)
-    {
-        if(username1 != null)
-        {
-            res.json({message: 'welcome, '+username1});
-            break;
-        }
+    async()=>{
+        await delay(100);
+        res.json({message: 'welcome, '+username1});
+        username1 = '';
     }
-    username1 = '';
-})
 
+    
+})
 app.listen(port, () => {
     console.log(`Server is up and running at port: ${port}`);
 });
