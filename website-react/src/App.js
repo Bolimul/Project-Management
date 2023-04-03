0417aa14085aae1c35f099c51f643e679af0d135
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-03 16:21:12 +0300

    Solving failed deploy 2

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -1,6 +1,8 @@
 
 import Navbar from "./Navbar";
 import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
 import "./Styles.css";
 import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
 
