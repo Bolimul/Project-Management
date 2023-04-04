4186d4765197d3e8e3960f6fff4cf52086ee20cc
Shiac <shiracohen1345@gmail.com>
2023-04-05 00:21:39 +0300

    update Register.js

--- website-react/src/Register.js
+++ website-react/src/Register.js
@@ -15,7 +15,20 @@ export const Register = (props) =>{
     
     return (
         
-        //Code paste 2
+        <div className="auth-form-container">
+            <h2>Register page</h2>
+            <form className="register-form" onSubmit={handleSubmit}>
+                <label htmlForm = "name"> Full name</label>
+                <input value = {name} onChange={(e) =>setname(e.target.value)} type="username"  placeholder="Full name"  id = "name"  name = "name"/>
+                <label htmlform = "username">username</label>
+                <input value = {username} onChange={(e) =>setUsername(e.target.value)} type="username" placeholder= "Enter your username here" id="username" name="username" /> 
+                <label htmlform = "password">password</label>
+                <input value = {password} onChange={(e) =>setPassword(e.target.value)} type = "password"  placeholder= "Enter your password here" id="password" name="password" /> 
+                <button type = "submit">Log In</button>
+            </form>
+            
+            <Link to="/login"><button className = "link-btn">Already have an account? Log in here.</button></Link>
+        </div>
     );
 }
 
