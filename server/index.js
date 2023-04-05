4af9629090f6e77ac2a5d96818d9c3f0466fb327
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-05 12:40:56 +0300

    m

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-var username1 = '';
-const delay = ms =>new Promise(resolve => setTimeout(resolve, ms));
+let username1 = '';
 app.post('/login', (req, res) => {
-    var {username, password} = req.body;
+    const { username, password } = req.body;
+    const password1 = password;
+    password1.length();
     username1 = username;
-    res.json({message: 'welcome, '+username1});
+    res.json({ message: `welcome, ${username1}` });
 });
 app.get('/login', (req, res) => {
-    res.json({message: 'welcome, '+username1});
+    res.json({ message: `welcome, ${username1}` });
     username1 = '';
 });
 app.listen(port, () => {
