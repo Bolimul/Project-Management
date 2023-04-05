f55e1f424ac674b0a2b26758699ff3582086c374
Nikita_Solonets <nikita.solonets@gmail.com>
2023-04-05 09:49:21 +0300

    Updating tests

---
+++ website-react/tests/App.spec.js
@@ -0,0 +1,16 @@
+function password_validation(password){
+    password_user = 'abc234!';
+    return password_user == password;
+  }
+    
+  
+  test('successful password validation',() =>{
+      const userPassword = 'abc234!';
+      expect(password_validation(userPassword)).to.be.eql(true);
+  });
+  
+  
+  test('unsuccessful password validation',() =>{
+    const password1 = 'a1111';
+    expect(password_validation(password1)).not.toBe(true);
+  });
