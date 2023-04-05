a6000a181cb4c8892e038faec2e782a3ea176a2c
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2023-04-05 08:45:47 +0300

    password tests

--- website-react/src/App.test.js
+++ website-react/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+function password_validation(password){
+  password_user = 'abc234!';
+  return password_user == password;
+}
+  
+
+test('successful password validation',() =>{
+    const userPassword = 'abc234!';
+    expect(password_validation(userPassword)).to.be.eql(true);
+});
+
+
+test('unsuccessful password validation',() =>{
+  const password1 = 'a1111';
+  expect(password_validation(password1)).not.toBe(true);
 });
