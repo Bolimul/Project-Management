960899e9e4f4470c0c46164953ca53bfa596a4b7
DoronSpitzer <doronspitzer601@gmail.com>
2023-05-18 20:24:04 +0300

    post func

--- website-react/src/Home.js
+++ website-react/src/Home.js
@@ -1,12 +1,41 @@
+import React, { useState } from "react";
 import "./HomeStyle.css";
 
-
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+  const handlePostSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const postContent = formData.get("content");
+
+    if (postContent.trim() !== "") {
+      const newPost = {
+        id: Date.now(),
+        content: postContent.trim(),
+        timestamp: new Date().toISOString(),
+      };
+
+      setPosts([newPost, ...posts]);
+      e.target.reset();
+    }
+  };
+
   return (
     <>
       <div className="home">
         <h1>HOME PAGE :-)</h1>
         <h3>WEB PROJECT</h3>
+        <form onSubmit={handlePostSubmit}>
+          <textarea name="content" placeholder="Add Post.." />
+          <button type="POST IT!">Post</button>
+        </form>
+        {posts.map((post) => (
+          <div key={post.id} className="post">
+            <p>{post.content}</p>
+            <small>{post.timestamp}</small>
+          </div>
+        ))}
       </div>
     </>
   );
