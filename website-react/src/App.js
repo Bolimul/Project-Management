d10a5068671bfba72668593e27930cfce3ffe96f
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2023-05-21 08:55:41 +0300

    add path to saved posts

--- website-react/src/App.js
+++ website-react/src/App.js
@@ -3,9 +3,11 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
+import PersonalAreaPostsSaved from "./PersonalAreaPostsSaved";
 
 import "./Styles.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import PersonalAreaPostsSaved from "./PersonalAreaPostsSaved";
 
 export default function App() {
   return (
@@ -16,6 +18,7 @@ export default function App() {
           <Route to="/" element={<Home />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />}/>
+          <Route path="/savedPosts" element={<PersonalAreaPostsSaved />}/>
         </Routes>
       </BrowserRouter>
     </div>
