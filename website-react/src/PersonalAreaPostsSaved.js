72d276cfcbb3f2386e8506d8b86f9dc065af4f6f
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2023-05-21 10:48:32 +0300

    update posts personal area

--- website-react/src/PersonalAreaPostsSaved.js
+++ website-react/src/PersonalAreaPostsSaved.js
@@ -1,41 +1,44 @@
-
-
 const  post_saved = [
-    {title_1: "New Research about the human genetics"},
-    {title_2: "Structure of heart"},
-    {title_3: "Common mistakes when trying the heal from cold"},
-    {title_4:"New Alergies this season"},
-    {title_5:"Protect your ears from the cold"}
+    "New Research about the human genetics",
+    "Structure of heart",
+    "Common mistakes when trying the heal from cold",
+    "New Alergies this season",
+    "Protect your ears from the cold"
 ];
 
 const post_liked = [
-    {title_1: "New technologies for cancer treatments"},
-    {title_2: "You are brushing your teeth wrong"},
-    {title_3: "Protect your eyes from the sun"},
-    {title_4:"Inovations in insoles"},
+    "New technologies for cancer treatments",
+    "You are brushing your teeth wrong",
+    "Protect your eyes from the sun",
+    "Inovations in insoles"
 
 ];
 
-function userSavedPosts(){
+function PersonalAreaPostsSaved(){
     return(
-        <div className="Posts you liked">
-           <h2>{post_saved[0]}</h2>
-           <h2>{post_saved[1]}</h2>
-           <h2>{post_saved[2]}</h2>
-           <h2>{post_saved[3]}</h2>
-           <h2>{post_saved[4]}</h2>
+                <div className='w-screen h-screen grid grid-rows-2 '>
+                    <div className="w-full h-full centered">
+                        <h1>Posts you Saved</h1>
+                        <h2>{post_saved[0]}</h2>
+                        <h2>{post_saved[1]}</h2>
+                        <h2>{post_saved[2]}</h2>
+                        <h2>{post_saved[3]}</h2>
+                        <h2>{post_saved[4]}</h2>
+       
+                    </div>
 
-           <div className = "Posts you liked"> 
-            <h2>{post_liked[0]}</h2>
-            <h2>{post_liked[1]}</h2>
-            <h2>{post_liked[2]}</h2>
-            <h2>{post_liked[3]}</h2>
-            <h2>{post_liked[4]}</h2> 
-            </div>
+                <div className="w-full h-full centered">
+                    <h1>Posts you Liked</h1>
+                    <h2>{post_liked[0]}</h2>
+                    <h2>{post_liked[1]}</h2>
+                    <h2>{post_liked[2]}</h2>
+                    <h2>{post_liked[3]}</h2>
+                    <h2>{post_liked[4]}</h2> 
+                </div>
 
-        </div>
+                </div>
+ 
     )
 }
 
-
-export default userSavedPosts()
+export default PersonalAreaPostsSaved;
