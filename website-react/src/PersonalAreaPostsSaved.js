f07e33083fad046b2753adff2c2878e58f5d5e9b
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2023-06-11 22:33:11 +0300

    posts in personal area

--- website-react/src/PersonalAreaPostsSaved.js
+++ website-react/src/PersonalAreaPostsSaved.js
@@ -1,28 +1,20 @@
 import {db,auth} from "./firebase"
-import {doc, getDoc} from "firebase/firestore";
+import {doc, getDoc, onSnapshot} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 
 
 
-var  post_saved = [];
+// var  posts_liked = [
+//     "New technologies for cancer treatments",
+//     "You are brushing your teeth wrong",
+//     "Protect your eyes from the sun",
+//     "Inovations in insoles"
+// ];
 
-const post_liked = [
-    "New technologies for cancer treatments",
-    "You are brushing your teeth wrong",
-    "Protect your eyes from the sun",
-    "Inovations in insoles"
 
-];
-
-const getData = async() => {
-    const docRef =  doc(db, 'users-profile-data', auth.currentUser.uid);
-    const userDoc =  await getDoc(docRef);
-    post_saved = await userDoc.data().savedPosts;
-    console.log(post_saved[0].PostTitle)
-}
 
 export function PersonalAreaPostsSaved(){
     // const auth = getAuth();
@@ -30,15 +22,42 @@ export function PersonalAreaPostsSaved(){
     // if (user != null) { 
     //     var savedUserPosts = user.savedPosts;
     // }
-    getData();
-     
+    var[posts_saved,setPosts_saved] = useState([]);
+    var [posts_liked,setPosts_liked] = useState([]);
+
+    const getData = async() => {
+        const docRef =  doc(db, 'users-profile-data', auth.currentUser.uid);
+        const userDoc = onSnapshot(docRef,async (doc)=>{
+            setPosts_saved(await doc.data().savedPosts);
+        setPosts_liked(await doc.data().LikedPosts);
+        });
+        // setPosts_saved(await userDoc.data().savedPosts);
+        // setPosts_liked(await userDoc.data().LikedPosts);
+        console.log(posts_saved[0].PostTitle)
+        console.log(posts_liked[0])
+    }
     
+    
+    useEffect(() => {
+        getData();
+        //doc(db, 'users-profile-data', auth.currentUser.uid)
+        
+       },[]);
+   
     return(
                 <div className='Saved Posts'>
                     
-                    <>Posts</>
-                    <></>
-
+                  <h1>Posts You Saved</h1>
+                  {posts_saved.map(post_s => <div>{post_s}</div>)}
+                  <h1>Posts You Liked</h1>
+                  {posts_liked.map(post_l => <div>{post_l}</div>)}
+                  {/* {posts_liked.map(post_l =>  
+                    <div>
+                           <p>Title:{post_l.title}</p> 
+                           <p>Topic:{post_l.topic}</p>
+                           <p>Content:{post_l.content}</p>
+                           <p>Image:{post_l.image}</p> 
+                        </div>)}  */}
                 </div>
  
     )
